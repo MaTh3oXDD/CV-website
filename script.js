093fe7f9b77f1dd382782207cbf4674307d5e171
unknown <Mateusz Jedrzejczak>
2024-08-16 16:58:30 +0200

    header

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    var navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('show');
+});
